ecd7b310971271a7c5ef56cc937a27a2235f4e59
André Wendt <awendt@freeshell.org>
2009-10-02 21:45:22 +0200

    Caching JSONP jqueries, use title attribute for photo title

--- jquery.23badge.js
+++ jquery.23badge.js
@@ -4,6 +4,8 @@ jQuery.fn.badge = function(config) {
     return (Math.round(Math.random())-0.5);
   };
 
+  jQuery.ajaxSetup({cache: true});
+
   var show_photos = function(data) {
     var photo_array = data.photos.photo;
     photo_array.sort(sort_random);
@@ -13,6 +15,7 @@ jQuery.fn.badge = function(config) {
           .replace(/{{server}}/, photo.server)
           .replace(/{{filename}}/, [photo.id, photo.secret, 's'].join('_')+'.jpg'))
         .attr("alt", photo.title)
+        .attr("title", photo.title)
         .attr("class", "badge-photo")
         .appendTo(elem.selector)
         .wrap('<a href="http://www.23hq.com/{{user}}/photo/{{photo}}"></a>'
